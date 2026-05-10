59dd33399c89f9599fcacffb78da70a9187b685d
code043 <procoder043@gmail.com>
2026-05-10 12:38:51 -0300

    feat: main navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <header className="bg-[#272727] text-white flex justify-between px-8 py-5 mb-2">
+      <div>Notes</div>
+      <div className="w-8 h-8 bg-blue-500 rounded-full"></div>
+    </header>
+  );
+}
