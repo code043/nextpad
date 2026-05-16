0e8c9ebb2db21bb7e6fe8a90cd18f1f72f550406
code043 <procoder043@gmail.com>
2026-05-16 15:41:15 -0300

    feat: notes grid

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -22,8 +22,8 @@ export default function DashboardLayout({
   if (!user) return null;
 
   return (
-    <div >
-      <nav>Dashboard</nav>
+    <div className="p-10 ">
+      <h1 className="text-center text-4xl font-bold tracking-tight leading-tight pb-20">Dashboard</h1>
       <main>{children}</main>
     </div>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,37 @@
-"use client"
-import { useNotas } from '@/hooks/useNotes';
-import Link from 'next/link';
+"use client";
+import { useNotas } from "@/hooks/useNotes";
+import Link from "next/link";
 
 export default function Dashboard() {
   const { notas, loading, error } = useNotas();
-
-if (loading || !notas) return <p>Carregando notas...</p>;
-if (error) return <p>{error}</p>;
-return (
-  <ul>
-    {notas.map((nota) => (
-        <div key={nota.id}>
-          <Link href={`/dashboard/note/${nota.id}`}>
-            <h2>{nota.title}</h2>
-          </Link>
-        </div>
-      ))}
-  </ul>
-)
+  function setDate(d: string) {
+    const date = new Date(d);
+    return date.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  }
+  if (loading || !notas) return <p>Carregando notas...</p>;
+  if (error) return <p>{error}</p>;
+  return (
+    <div>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {notas.map((nota) => (
+          <div
+            key={nota.id}
+            className="flex flex-col gap-4 p-4 rounded-lg shadow-sm bg-[#272727] text-white  w-100"
+          >
+            <Link href={`/dashboard/note/${nota.id}`}>
+              <h2 className="text-1xl font-bold tracking-tight">
+                {nota.title}
+              </h2>
+            </Link>
+            <p className="text-base leading-relaxed mt-4 ">{nota?.content}</p>
+            <p className="text-sm">{setDate(nota?.createdAt)}</p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
